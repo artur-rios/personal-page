60b4e7e8378a580d282b33b21d08c42561f8440d
Artur Rios <arturdev@duck.com>
2025-01-18 17:24:50 -0300

    fix: contact link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -87,7 +87,7 @@ export default function Navbar() {
                 <li key={link.title}>
                   <Link
                     className="hover:underline"
-                    href={lang === 'pt' ? link.ptHref || '' : link.href || ''}
+                    href={lang === 'pt' ? link.ptHref || link.href || '' : link.href || ''}
                     onClick={handleClick}
                   >
                     {lang === 'pt' && link.ptTitle ? link.ptTitle : link.title}
