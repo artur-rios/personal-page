3e8f2b0e56ed50dfed5558e2f60431acd313a6c5
Artur Rios <arturdev@duck.com>
2026-07-09 11:57:11 -0300

    feat: add Project type

---
+++ src/types/project.ts
@@ -0,0 +1,11 @@
+export type Project = {
+  title: string;
+  ptTitle?: string;
+  description: string;
+  ptDescription?: string;
+  websiteUrl?: string;
+  githubUrl: string;
+  repoOwner: string;
+  repoName: string;
+  tech: string[];
+};
