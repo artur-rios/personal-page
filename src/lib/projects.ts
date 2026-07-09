2d9a018bdc277782458a45e9d026882c01c4c945
Artur Rios <arturdev@duck.com>
2026-07-09 11:57:20 -0300

    feat: add projects data with placeholders

---
+++ src/lib/projects.ts
@@ -0,0 +1,36 @@
+import { Project } from '@/types/project';
+
+export const projects: Project[] = [
+  {
+    title: 'Project Alpha',
+    ptTitle: 'Projeto Alpha',
+    description: 'TODO: Add a short description of Project Alpha and what it does.',
+    ptDescription: 'TODO: Adicione uma breve descrição do Projeto Alpha e o que ele faz.',
+    websiteUrl: 'https://example.com',
+    githubUrl: 'https://github.com/artur-rios/project-alpha',
+    repoOwner: 'artur-rios',
+    repoName: 'project-alpha',
+    tech: ['TypeScript', 'React', 'Node.js'],
+  },
+  {
+    title: 'Project Beta',
+    ptTitle: 'Projeto Beta',
+    description: 'TODO: Add a short description of Project Beta and what it does.',
+    ptDescription: 'TODO: Adicione uma breve descrição do Projeto Beta e o que ele faz.',
+    githubUrl: 'https://github.com/artur-rios/project-beta',
+    repoOwner: 'artur-rios',
+    repoName: 'project-beta',
+    tech: ['Python', 'FastAPI', 'PostgreSQL'],
+  },
+  {
+    title: 'Project Gamma',
+    ptTitle: 'Projeto Gama',
+    description: 'TODO: Add a short description of Project Gamma and what it does.',
+    ptDescription: 'TODO: Adicione uma breve descrição do Projeto Gama e o que ele faz.',
+    websiteUrl: 'https://example.com',
+    githubUrl: 'https://github.com/artur-rios/project-gamma',
+    repoOwner: 'artur-rios',
+    repoName: 'project-gamma',
+    tech: ['C#', '.NET', 'Azure'],
+  },
+];
